ba04f8b976f41d57498f0fd8cb43bd3a5144dd80
rakarmp <raka_088@smkassalaambandung.sch.id>
2024-02-02 18:41:09 +0700

    Add io schedule

--- RSYB.c
+++ RSYB.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 void writeToFile(const char *filePath, const char *value)
 {
@@ -17,26 +18,51 @@ int isFolderExist(const char *folderPath)
     return access(folderPath, F_OK) == 0;
 }
 
+void setIOScheduler()
+{
+    FILE *schedulerFile = popen("find /sys/block/*/queue/scheduler", "r");
+    if (schedulerFile != NULL)
+    {
+        char schedulerPath[256];
+        while (fgets(schedulerPath, sizeof(schedulerPath), schedulerFile) != NULL)
+        {
+            schedulerPath[strcspn(schedulerPath, "\n")] = 0; // Remove newline character
+
+            writeToFile(schedulerPath, "bfq");
+            writeToFile(strcat(schedulerPath, "/iostats"), "0");
+            writeToFile(strcat(schedulerPath, "/add_random"), "0");
+            writeToFile(strcat(schedulerPath, "/nomerges"), "1");
+            writeToFile(strcat(schedulerPath, "/rq_affinity"), "0");
+            writeToFile(strcat(schedulerPath, "/rotational"), "0");
+            writeToFile(strcat(schedulerPath, "/read_ahead_kb"), "128");
+            writeToFile(strcat(schedulerPath, "/nr_requests"), "128");
+        }
+        pclose(schedulerFile);
+    }
+}
+
 int main()
 {
     const char *folderPath = "/data/adb/modules/RSYB";
     if (isFolderExist(folderPath))
     {
-        writeToFile("/proc/sys/vm/dirty_background_ratio", "20");
+        writeToFile("/proc/sys/vm/dirty_background_ratio", "10");
         writeToFile("/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", "interactive");
-        writeToFile("/sys/devices/system/cpu/cpufreq/performance/go_hispeed_load", "50");
-        writeToFile("/sys/devices/system/cpu/cpufreq/performance/above_hispeed_delay", "0");
-        writeToFile("/sys/devices/system/cpu/cpufreq/performance/boost", "0");
+        writeToFile("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load", "85");
+        writeToFile("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay", "20000");
+        writeToFile("/sys/devices/system/cpu/cpufreq/interactive/boost", "0");
         writeToFile("/sys/module/msm_performance/parameters/touchboost", "0");
-        writeToFile("/sys/devices/system/cpu/cpufreq/performance/max_freq_hysteresis", "1");
-        writeToFile("/sys/devices/system/cpu/cpufreq/performance/align_windows", "1");
+        writeToFile("/sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis", "0");
+        writeToFile("/sys/devices/system/cpu/cpufreq/interactive/align_windows", "1");
         writeToFile("/sys/devices/soc/1c00000.qcom,kgsl-3d0/devfreq/1c00000.qcom,kgsl-3d0/governor", "msm-adreno-tz");
         writeToFile("/sys/class/kgsl/kgsl-3d0/devfreq/governor", "msm-adreno-tz");
         writeToFile("/sys/module/adreno_idler/parameters/adreno_idler_active", "1");
-        writeToFile("/sys/module/lazyplug/parameters/nr_possible_cores", "8");
-        writeToFile("/dev/cpuset/foreground/cpus", "0-3,4-7");
-        writeToFile("/dev/cpuset/foreground/boost/cpus", "3-7");
-        writeToFile("/dev/cpuset/top-app/cpus", "0-7");
+        writeToFile("/sys/module/lazyplug/parameters/nr_possible_cores", "4");
+        writeToFile("/dev/cpuset/foreground/cpus", "0-1");
+        writeToFile("/dev/cpuset/foreground/boost/cpus", "1");
+        writeToFile("/dev/cpuset/top-app/cpus", "0-1");
+
+        setIOScheduler();
     }
     else
     {
